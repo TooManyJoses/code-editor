d818a88e720a9f1f7865111d0b48ee664015ca7f
José Rubio <jose.rubio@Davids-MacBook-Pro.local>
2021-08-10 09:45:34 -0400

    using esbuild to transform user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 function App() {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInput(event.target.value);
   };
 
-  const onClick = () => {
-    console.log(input)
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
   }
 
   return (
