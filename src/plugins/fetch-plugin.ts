cea5ec9d95500a4beccbdf12f1e3afd7daa5f708
José Rubio <jose.rubio@Davids-MacBook-Pro.local>
2021-08-11 11:52:26 -0400

    accounted for flatt css files and refactored the onloads

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -8,22 +8,50 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetchPlugin',
     setup(build: esbuild.PluginBuild) {
-    build.onLoad({ filter: /.*/ }, async (args: any) => {
-      console.log('onLoad', args);
 
-      if (args.path === 'index.js') {
-        return {
-          loader: 'jsx',
-          contents: inputCode,
-        };
-      }
+    build.onLoad({ filter: /(^index\.js$)/ }, async (args: any) => {
+      return {
+        loader: 'jsx',
+        contents: inputCode,
+      };
+    });
 
-      const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+  build.onLoad({ filter: /.*/ }, async (args: any) => {
+    const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
-      if (cachedResult) {
-        return cachedResult;
-      }
+    if (cachedResult) {
+      return cachedResult;
+    }
+  });
+
+    build.onLoad({ filter: /.css$/ }, async (args: any) => {
       const { data, request } = await axios.get(args.path);
+
+      const escaped = data
+        .replace(/\n/g, '')
+        .replace(/"/g, '\\" ')
+        .replace(/'/g, "\\'");
+      const contents =
+        `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+        `;
+
+      const result: esbuild.OnLoadResult = {
+        loader: 'jsx',
+        contents,
+        resolveDir: new URL('./', request.responseURL).pathname,
+      };
+
+      await fileCache.setItem(args.path, result);
+
+      return result;
+    });
+
+    build.onLoad({ filter: /.*/ }, async (args: any) => {
+      const { data, request } = await axios.get(args.path);
+
       const result: esbuild.OnLoadResult = {
         loader: 'jsx',
         contents: data,
