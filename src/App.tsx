749d70d134eba9978046a808cdfb63ca4c1980f1
Jose-Antonio D. Rubio <jose.rubio@levvel.io>
2021-08-16 16:03:36 -0400

    added in iframe with error messaging and resetting upon submission of code before the build

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,12 @@ function App() {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
   const ref = useRef<any>();
+  const iframe = useRef<any>();
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
 
@@ -28,6 +29,8 @@ function App() {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -41,9 +44,28 @@ function App() {
         'global': 'window',
       }
     });
-console.log(result)
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   }
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data)
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red"><h4>Run Time Error:</h4>'+ err +'</div>';
+              console.error(err);
+            }
+          }), false;
+        </script>
+      </body>
+    </html>
+  `
 
   return (
     <div>
@@ -52,8 +74,9 @@ console.log(result)
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe ref={iframe} sandbox="allow-scripts allow-modals" srcDoc={html}></iframe>
     </div>
   )
-}
+};
 
 export default App;
