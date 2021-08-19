4ca7683a2eff44048884e4a4ae6ff95842344589
Jose-Antonio D. Rubio <jose.rubio@levvel.io>
2021-08-19 15:44:58 -0400

    added in bulma and styling

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,40 +1,64 @@
-import { useRef } from 'react';
-import MonacoEditor, { EditorDidMount }from '@monaco-editor/react';
-import prettier from 'prettier';
-import parser from 'prettier/parser-babel';
+import { useRef } from "react";
+import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
+import codeShift from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
+import "./code-editor.css";
 
 interface CodeEditorProps {
-  initialValue: string,
+  initialValue: string;
   onChange(value: string): void;
-};
+}
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
   const editorRef = useRef<any>();
 
   const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
-    editorRef.current = monacoEditor
+    editorRef.current = monacoEditor;
     monacoEditor.onDidChangeModelContent(() => {
       console.log(getValue());
     });
 
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+
+    const highlighter = new Highlighter(
+      // @ts-ignore
+      window.monaco,
+      codeShift,
+      monacoEditor
+    );
+
+    highlighter.highLightOnDidChangeModelContent(
+      () => {},
+      () => {},
+      undefined,
+      () => {}
+    );
   };
 
   const onFormatClick = () => {
     const unformatted = editorRef.current.getModel().getValue();
-    const formatted = prettier.format(unformatted, {
-      parser: 'babel',
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      singleQuote: true
-    });
+    const formatted = prettier
+      .format(unformatted, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, "");
     editorRef.current.setValue(formatted);
-  }
+  };
 
   return (
-    <div>
-      <button onClick={onFormatClick}>Format</button>
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-warning is-small"
+        onClick={onFormatClick}
+      >
+        Prettify
+      </button>
       <MonacoEditor
         value={initialValue}
         editorDidMount={onEditorDidMount}
@@ -53,7 +77,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
         }}
       />
     </div>
-  )
-}
+  );
+};
 
-export default CodeEditor
+export default CodeEditor;
