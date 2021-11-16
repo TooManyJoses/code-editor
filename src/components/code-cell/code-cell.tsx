c438e355c83b408726d6d35046d2984f84200bd4
Iron.Snowflake <jadrubio@gmail.com>
2021-11-15 21:21:10 -0500

    added in preview progress for bundling loader and took care of preview element flicker

--- src/components/code-cell/code-cell.tsx
+++ src/components/code-cell/code-cell.tsx
@@ -5,6 +5,7 @@ import Resizable from '../resizable/resizable';
 import { Cell } from './../../state';
 import { useActions } from './../hooks/useActions';
 import { useTypedSelector } from './../hooks/useTypedSelector';
+import './code-cell.css';
 
 interface CodeCellProps {
   cell: Cell;
@@ -15,6 +16,10 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles![cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 750);
@@ -22,6 +27,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return () => {
       clearTimeout(timer);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.id, cell.content, createBundle]);
 
   return (
@@ -33,7 +39,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} err={bundle.err} />}
+        <div className="preview-wrapper">
+          {
+            !bundle || bundle.loading
+              ? <div className="progress-container">
+                <progress className="progress is-small is-primary" max="100">
+                  Loading
+                </progress>
+              </div>
+              : <Preview code={bundle.code} err={bundle.err} />
+          }
+        </div>
       </div>
     </Resizable>
   );
